8c15ee3274737415f203713c470ea0185f28c3f0
mona dabbah <monadabbah89@gmail.com>
2022-10-24 19:01:13 +0300

    modified projects

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -14,7 +14,7 @@ const Projects = () => {
                         <h5>Restaurant Website</h5>
                         <span className="projects__stack">HTML5 - CSS3 - JS</span>
                     </div>
-                    <a className="projects__button" href="https://github.com/monadabbah/Foody-Restaurant/archive/refs/heads/main.zip">Download</a> <i className="uil uil-arrow-down projects__button-icon"></i>
+                    <span className="projects__button"><a className="projects__button" href="https://github.com/monadabbah/Foody-Restaurant/archive/refs/heads/main.zip">Download</a> <i className="uil uil-arrow-down projects__button-icon"></i></span>
                 </div>
 
                 <div className="projects__content">
@@ -23,7 +23,7 @@ const Projects = () => {
                         <h5>NUNO Bootstrap Theme</h5>
                         <span className="projects__stack">HTML5 - CSS3 - Bootstrap</span>
                     </div>
-                    <a className="projects__button" href="https://github.com/monadabbah/Nuno-Theme/archive/refs/heads/main.zip">Download</a> <i className="uil uil-arrow-down projects__button-icon"></i>
+                    <span className="projects__button"><a className="projects__button" href="https://github.com/monadabbah/Nuno-Theme/archive/refs/heads/main.zip">Download</a> <i className="uil uil-arrow-down projects__button-icon"></i></span>
                 </div>
 
                 <div className="projects__content">
@@ -31,7 +31,7 @@ const Projects = () => {
                         <h5>Character Design</h5>
                         <span className="projects__stack">Illustrator</span>
                     </div>
-                    <a className="projects__button" href="https://drive.google.com/file/d/1kd-9hiO-CPpvfn2JS7ybk_7pMTBWpMUA/view?usp=sharing" target="_blank">Download</a> <i className="uil uil-arrow-down projects__button-icon"></i>
+                    <span className="projects__button"><a className="projects__button" href="https://drive.google.com/file/d/1kd-9hiO-CPpvfn2JS7ybk_7pMTBWpMUA/view?usp=sharing" target="_blank">Download</a> <i className="uil uil-arrow-down projects__button-icon"></i></span>
                 </div>
 
                 <div className="projects__content">
@@ -39,7 +39,7 @@ const Projects = () => {
                         <h5>Fish Restaurant Design</h5>
                         <span className="projects__stack">Illustrator - Photoshop</span>
                     </div>
-                    <a className="projects__button" href="https://drive.google.com/file/d/1G9GbOtUzeK5Vgg_gMGlP__hbn4C6XjuK/view?usp=sharing" target="_blank">Download</a> <i className="uil uil-arrow-down projects__button-icon"></i>
+                    <span className="projects__button"><a className="projects__button" href="https://drive.google.com/file/d/1G9GbOtUzeK5Vgg_gMGlP__hbn4C6XjuK/view?usp=sharing" target="_blank">Download</a> <i className="uil uil-arrow-down projects__button-icon"></i></span>
                 </div>
             </div>
         </section>
